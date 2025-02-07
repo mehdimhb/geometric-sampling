604ef38aff6531e9f4068581823dc55f6372e2d6
mehdimhb <mehdi.mohebbi23@gmail.com>
2025-02-08 01:56:05 +0330

    Add new test cases

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,16 +1,13 @@
-import unittest
 from geometric_sampling.structs import Sample
 
+def test_almost_zero():
+    r = Sample(1e-10, frozenset({1, 2, 3}))
+    assert r.almost_zero()
 
-class SampleTestCase(unittest.TestCase):
-    def test_almost_zero(self):
-        r = Sample(1e-10, frozenset({1, 2, 3}))
-        assert r.almost_zero()
-
-    def test_compare(self):
-        r1 = Sample(0.3, frozenset({1, 2}))
-        r2 = Sample(0.5, frozenset({2, 3}))
-        assert r1 < r2
-        assert r2 > r1
-        assert -r1 > -r2
-        assert -r2 < -r1
+def test_compare():
+    r1 = Sample(0.3, frozenset({1, 2}))
+    r2 = Sample(0.5, frozenset({2, 3}))
+    assert r1 < r2
+    assert r2 > r1
+    assert -r1 > -r2
+    assert -r2 < -r1
